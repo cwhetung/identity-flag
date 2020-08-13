3b9cdd05385087ba226ea6d43f4afc3a169a13d3
Clay Whetung <1455179+cwhetung@users.noreply.github.com>
2020-08-13 11:19:10 -0700

    Add student templates

---
+++ script.js
@@ -0,0 +1,18 @@
+/*global abs,angleMode,append,background,beginShape,bezier,box,camera,ceil,CENTER,color,cone,cos,createCanvas,createCanvas,createGraphics,curveVertex,cylinder,DEGREES,displayHeight,displayWidth,dist,div,DOWN_ARROW,ellipse,endShape,fill,floor,frameCount,frameRate,height,image,key,keyCode,keyIsDown,keyIsPressed,keyIsPressed,keyPressed,LEFT,LEFT_ARROW,lerpColor,line,loadImage,loadJSON,loadSound,map,mouseIsPressed,mouseX,mouseY,noFill,noLoop,normalMaterial,noStroke,p5,plane,point,pointLight,pop,push,push,RADIANS,radians,random,rect,rectMode,resizeCanvas,resizeCanvas,RIGHT,RIGHT_ARROW,rotate,rotateX,rotateY,rotateZ,round,round,scale,shuffle,sin,sphere,stroke,strokeWeight,text,textAlign,textFont,textSize,texture,textWidth,torus,translate,triangle,UP_ARROW,WEBGL,width,windowHeight,windowHeight,windowWidth,world */
+
+// create global variables up here
+
+// The setup function runs ONCE when the website loads
+function setup(){
+  createCanvas(500, 300);
+  background("white");
+  //noStroke();
+  angleMode(DEGREES)
+}
+
+// The draw function is called over and over again
+function draw(){
+  /* START OF YOUR CODE */
+  
+  /* END OF YOUR CODE */
+}
